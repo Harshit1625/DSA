dc2d3d7c49610c5f81b3e02cf6eb729a518f4012
Harshit Srivastava <harshitsri1625@gmail.com>
2024-12-21 21:28:28 +0530

    21DEC24

--- Backtracking/Backtracking.java
+++ Backtracking/Backtracking.java
@@ -18,8 +18,7 @@
 //     }
 // }
 
-//Time complexity will be n*n! as n would be the steps to every an answer multiplying it with n! which would be all the ans
-
+//Time complexity will be n*n! as n would be the steps to every an answer multiplying it with n! which would be all the ans....
 // ---------------------------------------------------------------------------------------------------------------------------------
 //N_Queen Problem
 
@@ -176,26 +175,79 @@
 // ---------------------------------------------------------------------------------------------------------------------------------
 //Sudoku Solver
 class Backtracking{
+   public boolean isSafe(char[][]board ,  int row , int col , int number){
+    //row & column
+    for(int i=0;i <board.length ; i++ ){
+        if(board[i][col] == (char)(number + '0')){
+            return false;
+        }
+        if(board[row][i] == (char)(number + '0')){
+            return false;
+        }
+    }
 
+    //grid
+    int sr = (row/3) *3;
+    int sc = (col/3) * 3;
+    
+    for(int i= sr ; i < sr + 3; i++){
+        for(int j = sc; i<sc +3; j++){
+           if(board[i][j] == (char)(number + '0')){
+            return false;
+           }
+        }
+    }
+    return true;
+   }
+
+    public boolean helper(char[][] board , int row , int col){
+        if(row == board.length){
+            System.out.println(board);
+            return true;
+        }
+
+        int newRow = 0;
+        int newCol = 0;
+        //we will increase the column and row untill it reaches the board length
+        if(col != board.length -1){
+            newRow = row;
+            newCol = col + 1;
+        }else{
+            newRow = row + 1;
+            newCol = col;
+        }
+
+        //checking if the number is present there or not
+        if(board[row][col] != '.'){
+            //calling this helper function recursively to check if the board is filled or not if the board is filled it will return true according to the base case 
+          if(helper(board, newRow, newCol)){
+            return true;
+          }
+          else{
+            for(int i = 0;i<=9 ; i++){
+             if(isSafe(board , row , col , i)){
+               board[row][col] = (char)(i + '0'); //typecasting into character
+               if(helper(board, newRow, newCol)){
+                return true;
+               }else {
+                board[row][col] = '.';
+               }
+             }
+            }
+          }
+        }
+        return false;
+    }
+
+    public void sudokuSolver(char[][] board){
+      helper(board, 0, 0);
+    }
     public static void main(String[] args) {
-        
+        char [][] board = new char[9][9];
+        Backtracking bc = new Backtracking();
+        bc.sudokuSolver(board);
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 // ---------------------------------------------------------------------------------------------------------------------------------
 //KnightL not done ??????
