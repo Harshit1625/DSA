843ca53e2988e299c2a4295e6b0045d9a7f21559
Harshit Srivastava <harshitsri1625@gmail.com>
2024-12-28 23:43:03 +0530

    28DEC24

--- Linked_List/PracticeSet.java
+++ Linked_List/PracticeSet.java
@@ -2,51 +2,51 @@ package Linked_List;
 
 //------------------------------------------------------------------------------------------------------
 //Q1 :- Find the nth node from last  
-class PracticeSet {
-    ListNode head;
+// class PracticeSet {
+//     ListNode head;
 
-    class ListNode {
-        int val; // val means data which we are passing
-        ListNode next;
+//     class ListNode {
+//         int val; // val means data which we are passing
+//         ListNode next;
 
-        ListNode(int val) {
-            this.val = val;
-        }
+//         ListNode(int val) {
+//             this.val = val;
+//         }
 
-        ListNode(int val, ListNode next) {
-            this.val = val;
-            this.next = next;
-        }
-    }
+//         ListNode(int val, ListNode next) {
+//             this.val = val;
+//             this.next = next;
+//         }
+//     }
 
-    public ListNode removeNthFromEnd(ListNode head, int n) {
-        if (head.next == null) {
-            return null;
-        }
+//     public ListNode removeNthFromEnd(ListNode head, int n) {
+//         if (head.next == null) {
+//             return null;
+//         }
 
-        // size
-        int size = 0;
-        ListNode curr = head;
-        while (curr != null) {
-            curr = curr.next;
-            size++;
-        }
+//         // size
+//         int size = 0;
+//         ListNode curr = head;
+//         while (curr != null) {
+//             curr = curr.next;
+//             size++;
+//         }
 
-        if (n == size) {
-            return head.next;
-        }
+//         if (n == size) {
+//             return head.next;
+//         }
 
-        // deletion of the node
-        int indexToSearch = size - n;
-        ListNode prev = head;
-        int i = 1; // Start from 1 since prev is already at head
-        while (i < indexToSearch) {
-            prev = prev.next;
-            i++;
-        }
-        prev.next = prev.next.next;
-        return head;
-    }
+//         // deletion of the node
+//         int indexToSearch = size - n;
+//         ListNode prev = head;
+//         int i = 1; // Start from 1 since prev is already at head
+//         while (i < indexToSearch) {
+//             prev = prev.next;
+//             i++;
+//         }
+//         prev.next = prev.next.next;
+//         return head;
+//     }
 
 // -----------------------------------------------------
 //Why Return head?
@@ -67,7 +67,125 @@ class PracticeSet {
 
 // ------------------------------------------------
 
-    // Helper method to add nodes to the list
+    // Helper method to add nodes to the list from the last
+//     public void addNode(int data){
+//         ListNode newNode = new ListNode(data);
+//         if(head == null){
+//           head = newNode;
+//           return;
+//         }
+        
+//         ListNode currNode = head;
+//         while(currNode.next !=null){
+//            currNode = currNode.next;
+//         }
+//         currNode.next = newNode;
+//     }
+
+//     // Helper method to display the list
+//     public void display() {
+//         ListNode temp = head;
+//         while (temp != null) {
+//             System.out.print(temp.val + " -> ");
+//             temp = temp.next;
+//         }
+//         System.out.println("null");
+//     }
+
+//     public static void main(String[] args) {
+//         PracticeSet list = new PracticeSet();
+
+//         // Adding nodes to the list
+//         list.addNode(1);
+//         list.addNode(2);
+//         list.addNode(3);
+//         list.addNode(4);
+//         list.addNode(5);
+        
+//         System.out.println("Original List:");
+//         list.display();
+
+//         // Removing the 2nd node from the end
+//         list.head = list.removeNthFromEnd(list.head, 2);
+
+//         System.out.println("List after removing 2nd node from the end:");
+//         list.display();
+//     }
+// }
+
+//------------------------------------------------------------------------------------------------------
+//------------------------------------------------------------------------------------------------------
+//Q2 :- Check if the Linked list is palindrome or not?
+class PracticeSet {
+        ListNode head;
+    
+        class ListNode {
+            int val; // val means data which we are passing
+            ListNode next;
+    
+            ListNode(int val) {
+                this.val = val;
+            }
+    
+            ListNode(int val, ListNode next) {
+                this.val = val;
+                this.next = next;
+            }
+        }
+        //function to find middleNode
+        //:- we can find the middleNode by two approaches , first is we can calculate the size of the list amd then divide it by two ,the second approach is turtle-hare approach
+        //:- turtle and hare are basically two pointers in which hare jumps 2 steps but tutrtle jumps for 1 step , and when hares next step becomes equal to null we return the 
+        //   turtle position and that will be the middle pf the list.(they must start alongside from the head)
+        public ListNode middleNodeFinder(ListNode head){
+            ListNode hare = head;
+            ListNode turtle = head;
+
+            while(hare.next != null && hare.next.next !=null){
+                hare = hare.next.next;
+                turtle = turtle.next;
+            }
+            return turtle;
+        }
+
+        //function to reversetheSecondHalf
+        public ListNode reverseSecondHalf(ListNode head){
+            ListNode prevNode = null;
+            ListNode currNode = head;
+            ListNode nextNode;
+
+            while(currNode != null){
+              nextNode = currNode.next;
+
+              //updation
+              currNode.next = prevNode;
+              prevNode = currNode;
+              currNode = nextNode;
+            }
+            return prevNode;
+        }
+
+        //function to check LL if is palindrome or not
+        public boolean isPalindrome(ListNode head){
+         if(head == null || head.next == null){
+            return true;
+         }   
+
+         ListNode middleNode = middleNodeFinder(head);
+         ListNode secondHalfStart = reverseSecondHalf(middleNode.next);
+         ListNode firstHalfStart = head;
+         while(secondHalfStart != null){
+            if(firstHalfStart.val != secondHalfStart.val){
+                return false;
+            }
+           firstHalfStart = firstHalfStart.next;
+           secondHalfStart = secondHalfStart.next;
+         }
+
+         return true;
+        }
+
+
+     //  Helper method to add nodes to the list from the last
     public void addNode(int data){
         ListNode newNode = new ListNode(data);
         if(head == null){
@@ -99,21 +217,21 @@ class PracticeSet {
         list.addNode(1);
         list.addNode(2);
         list.addNode(3);
-        list.addNode(4);
-        list.addNode(5);
+        list.addNode(2);
+        list.addNode(1);
         
         System.out.println("Original List:");
         list.display();
-
-        // Removing the 2nd node from the end
-        list.head = list.removeNthFromEnd(list.head, 2);
-
-        System.out.println("List after removing 2nd node from the end:");
-        list.display();
+       boolean result = list.isPalindrome(list.head);
+       System.out.println(result);
     }
+
 }
 
+
 //------------------------------------------------------------------------------------------------------
 //------------------------------------------------------------------------------------------------------
-//Q2 :- Check if the Linked list is palindrome or not?
+//Q3 :- 
+
+
 
