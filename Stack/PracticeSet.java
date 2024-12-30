64e98b0d067b931aa806d9dc434d238480b4b7fd
Harshit Srivastava <harshitsri1625@gmail.com>
2024-12-30 17:13:44 +0530

    30DEC24-2nd

--- Stack/PracticeSet.java
+++ Stack/PracticeSet.java
@@ -1,8 +1,44 @@
 package Stack;
+import java.util.*;
 
 // ---------------------------------------------------------------
-// Q1 :- Push at the bottom of the stack
+// Q1 :- Push at the bottom of the stack + Reversing the stack
 
 public class PracticeSet {
-    
+
+    public void pushAtBottom(int data , Stack<Integer> s){
+        if(s.isEmpty()){
+          s.push(data);
+          return;
+        }
+        int top = s.pop();
+        pushAtBottom(data, s);
+        s.push(top);
+
+    }
+
+    //firstly recursively it will pop all the elements then we will pushAtBottom fn which will at the elements in reverse order
+    public void reverse(Stack<Integer> s){
+        if(s.isEmpty()){
+            return;
+        }
+        int top = s.pop();
+        reverse(s);
+        pushAtBottom(top, s);
+    }
+
+    public static void main(String[] args) {
+        Stack<Integer> st = new Stack<>();
+        PracticeSet ps = new PracticeSet();
+        st.push(1);
+        st.push(2);
+        st.push(3);
+        st.push(4);
+        ps.reverse(st);
+
+        while(!st.isEmpty()){
+            System.out.println(st.peek());
+            st.pop();
+        }   
+    }
 }
