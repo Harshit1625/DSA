98fad6bdd11550f03b86e47c70ff37fb7bd5046e
Harshit Srivastava <harshitsri1625@gmail.com>
2025-01-11 15:22:52 +0530

    11DEC24

--- Hashing/HashMapImplementation.java
+++ Hashing/HashMapImplementation.java
@@ -1,10 +1,13 @@
 package Hashing;
 
-import java.util.*;
+import java.util.LinkedList;
+import java.util.ArrayList;
 
 //================================================================
 //in Java , we can make HashMap using ArrayOfLinkedList
 //Hashing means converting a form of a particular data into another form 
+//================================================================
+//in worst case the complexity will be O(n) , in avg case it will be O(1);
 //================================================================
 
 public class HashMapImplementation {
@@ -45,13 +48,42 @@ public class HashMapImplementation {
             int bucketIndex = key.hashCode(); // inbuilt function in java that returns a positive or negative value
             // but we need positive value only , and it should be below the length of
             // bucket (like here it should be less than N);
-            // Math.abs returns a positive of a number and (% N) will ensure that the value should be below N
+            // Math.abs returns a positive of a number and (% N) will ensure that the value
+            // should be below N
             return Math.abs(bucketIndex) % N;
         }
 
         // search function linked list to get the index at which the key is present
         public int searchInLL(K key, int bucketIndex) {
-         
+            LinkedList<Node> ll = buckets[bucketIndex];
+
+            for (int i = 0; i < ll.size(); i++) {
+                if (ll.get(i).key == key) {
+                    return i;
+                }
+            }
+            return -1;
+
+        }
+
+        // rehashing function
+        @SuppressWarnings("unchecked")
+        private void rehashingFn() {
+            LinkedList<Node> oldBucket[] = buckets;
+            buckets = new LinkedList[N * 2]; // we have created a new bucket , so we have to add LL initially
+
+            // adding LL initially
+            for (int i = 0; i < buckets.length; i++) {
+                buckets[i] = new LinkedList<>();
+            }
+
+            for (int i = 0; i < oldBucket.length; i++) {
+                LinkedList<Node> ll = oldBucket[i];
+                for (int j = 0; j < ll.size(); j++) {
+                    Node node = ll.get(i);
+                    put(node.key, node.value);
+                }
+            }
         }
 
         public void put(K key, V value) {
@@ -69,31 +101,86 @@ public class HashMapImplementation {
 
             // Rehashing :- means as the nodes increase , we have to increase the array size
             // too; to maintain the complexity
-            double lambda = (double) n / N; // lambda refers to the size of linkedlist (i.e ; lambda = n/N)
+            double lambda = (double) n / N; // lambda basically refers to the size of linkedlist (i.e ; lambda = n/N)
+            // as the number no of nodes increase , lambda will increase ; and if it
+            // increseas it will affect the complexity ; so to fix that we set a constant ,
+            // value like here it is 2.0 ; and if lambda become greater than that we will
+            // rehash;
             if (lambda > 2.0) {
-
+                rehashingFn();
             }
 
         }
 
         public boolean containsKey(K key) {
-            return false;
+            int bucketIndex = hashFunction(key);
+            int linkedlistIndex = searchInLL(key, bucketIndex); // checking if the key is already present or not
+
+            // -1 means the key is not present
+            if (linkedlistIndex == -1) {
+                return false;
+            } else {
+                return true;
+            }
         }
 
         public V remove(K key) {
-            return null;
+            int bucketIndex = hashFunction(key);
+            int linkedlistIndex = searchInLL(key, bucketIndex); // checking if the key is already present or not
+
+            // -1 means the key is not present
+            if (linkedlistIndex == -1) {
+                return null;
+            } else {
+                Node data = buckets[bucketIndex].remove(linkedlistIndex);
+                n--;
+                return data.value;
+            }
         }
 
         public V get(K key) {
-            return null;
+            int bucketIndex = hashFunction(key);
+            int linkedlistIndex = searchInLL(key, bucketIndex); // checking if the key is already present or not
+
+            // -1 means the key is not present
+            if (linkedlistIndex == -1) {
+                return null;
+            } else {
+                Node data = buckets[bucketIndex].get(linkedlistIndex);
+                return data.value;
+            }
         }
 
         public ArrayList<K> keySet() {
-            return null;
+            ArrayList<K> keys = new ArrayList<>();
+            for (int i = 0; i < buckets.length; i++) {
+                LinkedList<Node> ll = buckets[i];
+                for (int j = 0; j < ll.size(); j++) {
+                    Node node = ll.get(j);
+                    keys.add(node.key);
+                }
+            }
+            return keys;
         }
 
         public boolean isEmpty() {
-            return false;
+            return n == 0;
         }
+    }
+
+    public static void main(String[] args) {
+        HashMap<String, Integer> hm = new HashMap<>();
+        hm.put("India", 120);
+        hm.put("Pakistan", 130);
+        hm.put("China", 140);
+        System.out.println(hm.get("India"));
+        System.out.println(hm.keySet());
+
+        ArrayList<String> keys = hm.keySet();
+
+        for (int i = 0; i < keys.size(); i++) {
+            System.out.println(keys.get(i) + " : " + hm.get(keys.get(i)));
+        }
+
     }
 }
