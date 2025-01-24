69f06078f07598ac088516b9602a2e5724807f57
Harshit Srivastava <harshitsri1625@gmail.com>
2025-01-24 17:11:37 +0530

    24JAN25

--- Trie/Practice.java
+++ Trie/Practice.java
@@ -1,8 +1,6 @@
 package Trie;
 
-// -----------------------------------------------------------------
-// Word Break Problem
-//Given an input string and a dictionary of words , find out if the input string can be broken into a space separated sequence of dictionary words
+// ================================================================================================
 public class Practice {
     static class Node {
         Node[] children;
@@ -51,10 +49,78 @@ public class Practice {
         return true;
     }
 
-    public static void main(String[] args) {
-        String words[] = { "the", "ab", "there", "their", "any" };
+    // ===============================================================================================================
+    // Word Break Problem
+    // Given an input string and a dictionary of words , find out if the input
+    // string can be broken into a space separated sequence of dictionary words
+
+    // fn for wordbreak
+    // public static boolean wordBreak(String word) {
+    // if (word.length() == 0) {
+    // return true;
+    // }
+    // for (int i = 1; i <= word.length(); i++) {
+    // String firstPart = word.substring(0, i);
+    // String secondPart = word.substring(i);
+
+    // /*
+    // * Untill Search() returns true the recursive wont get called (i.e when we
+    // will
+    // * reach the end of word only then Search will be true)
+    // */
+
+    // // Since "sam" followed by "sung" did not successfully break the word,
+    // because
+    // // "sung" wasnt present in trie so we
+    // // backtrack to the previous call (wordBreak("samsung")) and continue the
+    // loop.
+    // if (Search(firstPart) && wordBreak(secondPart)) {
+    // return true;
+    // }
+    // }
+    // return false;
+    // }
+
+    // public static void main(String[] args) {
+    // String words[] = { "i", "like", "sam", "samsung", "mobile" };
+    // String key = "ilikesamsung";
+    // for (int i = 0; i < words.length; i++) {
+    // insert(words[i]);
+    // }
+    // System.out.println(wordBreak(key));
+
+    // }
+    // }
+
+    // -----------------------------------------------------------------
+    // Starts With Problem
+    // Returns true if there is a previously inserted string word that has the
+    // prefix, and false otherwise
+
+    public static boolean startsWith(String prefix) {
+        Node curr = root;
+        for (int i = 0; i < prefix.length(); i++) {
+            int idx = prefix.charAt(i) - 'a';
+            if (curr.children[idx] == null) {
+                return false;
+            }
+            curr = curr.children[idx];
+        }
+        return true;
+    }
+
+    public static void main(String args[]) {
+        String words[] = { "app", "apple", "mango", "man", "woman" };
+
         for (int i = 0; i < words.length; i++) {
             insert(words[i]);
         }
+        System.out.println(startsWith("app"));
     }
+
 }
+
+// -----------------------------------------------------------------
+// Count Unique Substrings
+// Given a string of length n of lowercase alphabet characters , we need to
+// count total number of distinct substrings of this string
